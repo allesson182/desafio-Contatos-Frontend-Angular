6e916e88f65cb08b9e7e224a38dd2d28d30cf633
allesson <allesson@usto.re>
2024-05-20 10:01:50 -0300

    Projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContatosComponent} from "./contatos/contatos.component";
+import {CadastroComponent} from "./cadastro/cadastro.component";
+import {AuthGuard} from "./guard/AuthGuard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: CadastroComponent, canActivate: [AuthGuard]},
+  { path: 'contatos', component: ContatosComponent, canActivate: [AuthGuard]},
+  { path: '', redirectTo: '/contatos', pathMatch: 'full'}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
+import {HttpService} from "./service/http.service";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import { EditDialogComponent } from './contatos/edit-dialog/edit-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    ContatosComponent,
+    EditDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    NgbModule,
+    MatIconModule,
+    MatTableModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
